e5fb24e88edbaeae7ea7ff7aa4ae351ac22a7864
akkimahesh <maheshakki799@gmail.com>
2025-12-05 21:47:06 +0530

    initial commit

--- security-groups.tf
+++ security-groups.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "ec2-sg" {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
-    security_groups = [aws_security_group.ALB-SG.id]
+    security_groups = [aws_security_group.alb-sg.id]
   }
 
   egress {
